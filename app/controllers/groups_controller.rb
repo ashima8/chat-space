414048d7a1b336b788de9515503b935533f1787b
ashima8 <ve_sha_t@yahoo.co.jp>
2019-08-16 18:13:34 +0900

    メッセージ機能

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,5 +1,5 @@
 class GroupsController < ApplicationController
-  before_action :set_group, only: [:edit, :update]
+  #before_action :set_group, only: [:edit, :update]
 
   def index
   end
@@ -18,6 +18,9 @@ class GroupsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
   def update
     if @group.update(group_params)
       redirect_to group_messages_path(@group), notice: 'グループを編集しました'
@@ -27,6 +30,7 @@ class GroupsController < ApplicationController
   end
 
   private
+
   def group_params
     params.require(:group).permit(:name, {:user_ids => [] })
   end
